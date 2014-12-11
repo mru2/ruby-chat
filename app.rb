36d7cf843204d2b8aa77bfdea41dc74f8e3b6a6d
David Ruyer <david.ruyer@jobteaser.com>
2014-12-11 18:31:33 +0100

    Ping pong

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class App < Sinatra::Base
   get '/join_room' do
     content_type 'text/event-stream'
 
+    stream(:keep_open) do |connection|
+      connection << "data: Welcome to the chat!\n\n" # Will be sent multiple times if no keep-alive frames are sent through the connection. Will be fixed in v1 anyway ^^
+    end
+
     puts 'Somebody joined the chat!'
   end
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,18 @@
 var Chat = {
   start: function(){
+    console.log('Joining the chat...');
     var source = new EventSource('/join_room');
+
+    var self = this;
+    source.addEventListener('message', function(event){
+      self.appendMessage({text: event.data});
+    });
+  },
+
+  appendMessage: function(message){
+    var line = $('<p>');
+    line.html(message.text);
+    $('#chatroom').append(line);
   }
 };
 
